ddf65b751f2b56b9d88e70309235cc1511499d73
Remco de Boer <redeboer@gmx.com>
2021-04-15 09:44:10 +0200

    ci: equalize dev environment with other ComPWA repos

--- .constraints/upgrade.sh
+++ .constraints/upgrade.sh
@@ -1,7 +1,7 @@
-# https://github.com/jazzband/pip-tools/issues/625
+# Freeze all requirements for this Python version in a constraints file
+# https://pip.pypa.io/en/stable/user_guide/#constraints-files
 
 PYTHON_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
-
 if [[ -z "$PYTHON_VERSION" ]]; then
     echo -e "\e[31;1mERROR: environment variable PYTHON_VERSION needs to be defined!\e[0m"
     exit 1
