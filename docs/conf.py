b6ffc01ebfe3327b79dc06bc6b2d52ae55464a15
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-01-03 10:30:26 +0100

    fix: redirect jsonschema v4.3.3

--- docs/conf.py
+++ docs/conf.py
@@ -225,7 +225,10 @@ nitpick_ignore = [
 
 # Intersphinx settings
 version_remapping = {
-    "jsonschema": {"4.3.2": "4.3.1"},
+    "jsonschema": {
+        "4.3.2": "4.3.1",
+        "4.3.3": "4.3.1",
+    },
 }
 
 
