eb61c977ccb79384db1e503e912648555b7de9ab
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-02-21 11:29:26 +0100

    docs: sort API by position in the source code

--- docs/conf.py
+++ docs/conf.py
@@ -174,6 +174,7 @@ autodoc_default_options = {
         ]
     ),
 }
+autodoc_member_order = "bysource"
 codeautolink_concat_default = True
 AUTODOC_INSERT_SIGNATURE_LINEBREAKS = True
 graphviz_output_format = "svg"
