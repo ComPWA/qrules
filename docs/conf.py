82e7df42cf3fb38c330a15b1c5b640dcee00b054
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-05-16 09:55:41 +0100

    ci: update pip constraints and pre-commit config (#177)
    * ci: allow cSpell compound words
      https://results.pre-commit.ci/run/github/355222824/1652689685.Zk6tf1jxT2m3YsMIuEBcNQ
    * fix: relink jsonschema API
    
    Co-authored-by: GitHub <noreply@github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -248,6 +248,7 @@ nitpick_ignore_regex = [
 # Intersphinx settings
 version_remapping = {
     "jsonschema": {
+        "4.5.1": "4.3.1",
         "4.3.2": "4.3.1",
         "4.3.3": "4.3.1",
         "4.4.0": "4.3.1",
