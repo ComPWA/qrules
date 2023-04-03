9f9fb60dcf88554bab962577193453c3beeab8fb
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-04-03 22:23:52 +0200

    MAINT: extend mass leak factor in Lc to pKpi

--- tests/channels/test_lc_to_p_km_pip.py
+++ tests/channels/test_lc_to_p_km_pip.py
@@ -7,7 +7,7 @@ def test_resonances():
         initial_state=["Lambda(c)+"],
         final_state=["p", "K-", "pi+"],
         allowed_intermediate_particles=["Delta", "K", "Lambda"],
-        mass_conservation_factor=0,
+        mass_conservation_factor=0.6,
     )
     stm.set_allowed_interaction_types([InteractionType.STRONG], node_id=1)
     problem_sets = stm.create_problem_sets()
@@ -17,8 +17,14 @@ def test_resonances():
         "Delta(1232)++",
         "Delta(1600)++",
         "Delta(1620)++",
+        "Delta(1900)++",
+        "Delta(1910)++",
+        "Delta(1920)++",
         "K(0)*(700)~0",
         "K*(892)~0",
+        "K*(1410)~0",
+        "K(0)*(1430)~0",
+        "Lambda(1405)",
         "Lambda(1600)",
         "Lambda(1670)",
         "Lambda(1810)",
