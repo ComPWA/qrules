6635348a952be5ff489be4182acf6a1cf7a1123b
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-01-21 00:02:51 +0100

    DOC: switch to source code link button (#246)
    * DX: switch to Python 3.9 in developer environment
    * MAINT: remove docstring from `conf.py`
    * MAINT: remove redundant `master_doc` and `source_suffix`
    * MAINT: sort `html_theme_options`

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,3 @@
-"""Configuration file for the Sphinx documentation builder.
-
-This file only contains a selection of the most common options. For a full list see the
-documentation: https://www.sphinx-doc.org/en/master/usage/configuration.html
-"""
-
 from __future__ import annotations
 
 import os
@@ -225,14 +219,6 @@ html_theme_options = {
             "type": "url",
         },
     ],
-    "logo": {"text": REPO_TITLE},
-    "repository_url": f"https://github.com/{ORGANIZATION}/{REPO_NAME}",
-    "repository_branch": BRANCH,
-    "path_to_docs": "docs",
-    "use_download_button": True,
-    "use_edit_page_button": True,
-    "use_issues_button": True,
-    "use_repository_button": True,
     "launch_buttons": {
         "binderhub_url": "https://mybinder.org",
         "colab_url": "https://colab.research.google.com",
@@ -241,8 +227,17 @@ html_theme_options = {
         "thebe": True,
         "thebelab": True,
     },
+    "logo": {"text": REPO_TITLE},
+    "path_to_docs": "docs",
+    "repository_branch": BRANCH,
+    "repository_url": f"https://github.com/{ORGANIZATION}/{REPO_NAME}",
     "show_navbar_depth": 2,
     "show_toc_level": 2,
+    "use_download_button": False,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_source_button": True,
 }
 html_title = REPO_TITLE
 intersphinx_mapping = {
@@ -262,7 +257,6 @@ linkcheck_ignore = [
     "https://doi.org/10.1002/andp.19955070504",  # 403 for onlinelibrary.wiley.com
 ]
 project = REPO_TITLE
-master_doc = "index"
 modindex_common_prefix = [f"{PACKAGE}."]
 myst_enable_extensions = [
     "amsmath",
@@ -301,11 +295,6 @@ primary_domain = "py"
 project = "QRules"
 pygments_style = "sphinx"
 release = get_package_version(PACKAGE)
-source_suffix = {
-    ".ipynb": "myst-nb",
-    ".md": "myst-nb",
-    ".rst": "restructuredtext",
-}
 suppress_warnings = [
     "myst.domains",
     # skipping unknown output mime type: application/json
