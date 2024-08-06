c18d411902ebc343fd009da7922e7115c28cb35d
grayson-helmholz <bero.verfuerth@rub.de>
2024-08-06 11:32:39 +0200

    inlined fixture-arguments, function-renaming

--- tests/unit/test_solving.py
+++ tests/unit/test_solving.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import copy
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import attrs
 import pytest
@@ -11,34 +11,49 @@ import qrules.quantum_numbers
 import qrules.solving
 import qrules.system_control
 import qrules.transition
-from qrules.argument_handling import GraphEdgePropertyMap, GraphNodePropertyMap, Rule
 from qrules.conservation_rules import (
     GraphElementRule,
+    isospin_validity,
     parity_conservation,
     spin_magnitude_conservation,
+    spin_validity,
 )
 from qrules.quantum_numbers import EdgeQuantumNumbers, NodeQuantumNumbers
 
+if TYPE_CHECKING:
+    from qrules.argument_handling import (
+        GraphEdgePropertyMap,
+        GraphNodePropertyMap,
+        Rule,
+    )
 
-def test_solve(
+
+def test_solve_with_new_quantum_number_problem_set(
     particle_database: qrules.particle.ParticleCollection,
     qn_problem_set: qrules.solving.QNProblemSet,
-    graph_edge_property_map: GraphEdgePropertyMap,
-    graph_node_property_map: GraphEdgePropertyMap,
-    edge_rules: set[GraphElementRule],
-    node_rules: set[Rule],
-    edge_domains: dict[Any, list],
-    node_domains: dict[Any, list],
-):
+) -> None:
     allowed_intermediate_states = [
         qrules.system_control.create_edge_properties(part) for part in particle_database
     ]
     solver = qrules.solving.CSPSolver(allowed_intermediate_states)
-    new_qn_problem_set = qnp_with_new_properties(
-        qn_problem_set, graph_edge_property_map, graph_node_property_map
+    new_qn_problem_set = quantum_number_problem_set_with_new_properties(
+        qn_problem_set,
+        graph_edge_property_map={
+            EdgeQuantumNumbers.spin_magnitude: 1,
+            EdgeQuantumNumbers.parity: -1,
+            EdgeQuantumNumbers.c_parity: 1,
+        },
+        graph_node_property_map={
+            NodeQuantumNumbers.s_magnitude: 1,
+            NodeQuantumNumbers.l_magnitude: 0,
+        },
     )
-    new_qn_problem_set = qnp_with_new_settings(
-        new_qn_problem_set, edge_rules, node_rules, edge_domains, node_domains
+    new_qn_problem_set = quantum_number_problem_set_with_new_settings(
+        new_qn_problem_set,
+        edge_rules={spin_validity, isospin_validity},
+        node_rules={spin_magnitude_conservation, parity_conservation},
+        edge_domains={"spin_magnitude": [1], "parity": [-1, 1], "c_parity": [-1, 1]},
+        node_domains={"l_projection": [0]},
     )
     solutions = solver.find_solutions(new_qn_problem_set)
 
@@ -56,7 +71,16 @@ def test_inner_dicts_unchanged(
     old_inner_graph_node_property_map = copy.deepcopy(
         qn_problem_set.initial_facts.interactions
     )
-    qnp_with_new_properties(
+    graph_edge_property_map = {
+        EdgeQuantumNumbers.spin_magnitude: 1,
+        EdgeQuantumNumbers.parity: -1,
+        EdgeQuantumNumbers.c_parity: 1,
+    }
+    graph_node_property_map = {
+        NodeQuantumNumbers.s_magnitude: 1,
+        NodeQuantumNumbers.l_magnitude: 0,
+    }
+    quantum_number_problem_set_with_new_properties(
         qn_problem_set, graph_edge_property_map, graph_node_property_map
     )
     assert old_inner_graph_edge_property_map == qn_problem_set.initial_facts.states
@@ -65,7 +89,7 @@ def test_inner_dicts_unchanged(
     )
 
 
-def qnp_with_new_settings(
+def quantum_number_problem_set_with_new_settings(
     qn_problem_set: qrules.solving.QNProblemSet,
     edge_rules: set[GraphElementRule],
     node_rules: set[Rule],
@@ -121,7 +145,7 @@ def qnp_with_new_settings(
     )
 
 
-def qnp_with_new_properties(
+def quantum_number_problem_set_with_new_properties(
     qn_problem_set: qrules.solving.QNProblemSet,
     graph_edge_property_map: GraphEdgePropertyMap,
     graph_node_property_map: GraphNodePropertyMap,
@@ -161,37 +185,3 @@ def qn_problem_set() -> qrules.solving.QNProblemSet:
         p.to_qn_problem_set() for pl in problem_sets.values() for p in pl
     ]
     return qn_problem_sets[0]
-
-
-@pytest.fixture(scope="session")
-def graph_edge_property_map() -> GraphEdgePropertyMap:
-    return {
-        EdgeQuantumNumbers.spin_magnitude: 1,
-        EdgeQuantumNumbers.parity: -1,
-        EdgeQuantumNumbers.c_parity: 1,
-    }
-
-
-@pytest.fixture(scope="session")
-def graph_node_property_map() -> GraphNodePropertyMap:
-    return {NodeQuantumNumbers.s_magnitude: 1, NodeQuantumNumbers.l_magnitude: 0}
-
-
-@pytest.fixture(scope="session")
-def edge_rules() -> set[GraphElementRule]:
-    return {spin_magnitude_conservation, parity_conservation}
-
-
-@pytest.fixture(scope="session")
-def node_rules() -> set[Rule]:
-    return {spin_magnitude_conservation, parity_conservation}
-
-
-@pytest.fixture(scope="session")
-def edge_domains() -> dict[Any, list]:
-    return {"spin_magnitude": [1], "parity": [-1, 1], "c_parity": [-1, 1]}
-
-
-@pytest.fixture(scope="session")
-def node_domains() -> dict[Any, list]:
-    return {"l_projection": [0]}
