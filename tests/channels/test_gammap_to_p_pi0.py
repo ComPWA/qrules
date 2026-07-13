f3cdd7892db102aa272e9fe78ab08d5613dd2934
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2026-07-13 09:02:59 +0200

    FEAT: classify transitions by Mandelstam channel

--- tests/channels/test_gammap_to_p_pi0.py
+++ tests/channels/test_gammap_to_p_pi0.py
@@ -30,3 +30,32 @@ def test_pi0_photoproduction(formalism: SpinFormalism, particle_database):
         "rho(770)0",
     ]
     assert {p.name for p in reaction.initial_state.values()} == {"gamma", "p"}
+
+
+def test_group_by_channel(particle_database):
+    reaction = qrules.generate_transitions(
+        initial_state=["gamma", "p"],
+        final_state=["pi0", "p"],
+        allowed_intermediate_particles=[
+            "Delta(1232)",
+            "N(1440)",
+            "rho(770)",
+            "omega(782)",
+        ],
+        allowed_interaction_types=["strong", "em"],
+        formalism="helicity",
+        particle_db=particle_database,
+    )
+    channels = {
+        channel: {
+            state.particle.name
+            for transition in transitions
+            for state in transition.intermediate_states.values()
+        }
+        for channel, transitions in reaction.group_by_channel().items()
+    }
+    assert channels == {
+        "s": {"Delta(1232)+", "N(1440)+"},
+        "t": {"omega(782)", "rho(770)0"},
+        "u": {"Delta(1232)+", "Delta(1232)~-", "N(1440)+", "N(1440)~-"},
+    }
