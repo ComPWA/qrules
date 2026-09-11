b13c06c147d0741b1c1ee3a9dae74f54095f1937
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2026-09-11 15:24:45 +0200

    MAINT: minor formatting improvements

--- src/qrules/particle.py
+++ src/qrules/particle.py
@@ -501,9 +501,8 @@ def load_pdg(*, source: PyPIPackage = "particle") -> ParticleCollection:
     """Create a `.ParticleCollection` with all entries from the PDG.
 
     By default, particle definitions are imported from the `particle
-    <https://github.com/scikit-hep/particle>`_ package. Set ``source="pdg"`` to
-    import them from the official `PDG Python API <https://pdgapi.lbl.gov/doc/>`_
-    instead.
+    <https://github.com/scikit-hep/particle>`_ package. Set ``source="pdg"`` to import
+    them from the official `PDG Python API <https://pdgapi.lbl.gov/doc/>`_ instead.
     """
     if source == "pdg":
         from qrules._pdg_adapter import load_pdg as load_official_pdg  # ruff: ignore[import-outside-top-level]
